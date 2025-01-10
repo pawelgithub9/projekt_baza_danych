cab7db27e16fd021e493bb81efe21e77fdb9330b
Droxxon <126577407+Droxxon@users.noreply.github.com>
2025-01-10 15:00:20 +0100

    Add files via upload

---
+++ pliki/mieszkania.sql
@@ -0,0 +1,11 @@
+create table mieszkania (
+	id autoincrement,
+	adres varchar(100),
+	liczbapokoi int,
+	metraz decimal(3,2),
+	pietro int,
+	typbudynku varchar(20),
+	umeblowanie bit,
+	constraint pk_mieszkania primary key (id),
+	constraint liczba_metraz check (metraz > 0)
+);

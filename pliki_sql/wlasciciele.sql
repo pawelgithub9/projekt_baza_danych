92583c8b59de43540153c142a5c2d2f2645884af
pawelgithub9 <pawelkonto1233@gmail.com>
2025-01-10 22:51:09 +0100

    Update wlasciciele.sql

--- pliki_sql/wlasciciele.sql
+++ pliki_sql/wlasciciele.sql
@@ -6,7 +6,7 @@ create table WLASCICIELE (
 	plec varchar(1),
 	telefon varchar(15),
 	adreszamieszkania varchar(100),
-	constraint pk_wlasciele primary key (id),
-	constraint plec_wlasciele check (plec in ('K', 'M')),
+	constraint pk_wlasciciele primary key (id),
+	constraint plec_wlasciciele check (plec in ('K', 'M')),
 	constraint pesel_wlasciciele check (len(pesel) = 11)
 )

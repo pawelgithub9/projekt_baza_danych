9fa4914a5dad90f7e5aa50ffd55227bbeee5e6c0
pawelgithub9 <pawelkonto1233@gmail.com>
2025-01-10 22:47:10 +0100

    Add files via upload

---
+++ pliki_sql/oplaty.sql
@@ -0,0 +1,11 @@
+create table OPLATY (
+	id autoincrement,
+	mieszkanie int,
+	rodzajoplaty varchar(7),
+	kwota decimal(4, 2),
+	data date,
+	constraint pk_oplaty primary key (id),
+	constraint fk_mieszkanie foreign key (mieszkanie) references MIESZKANIA (id),
+	constraint rodzajoplaty_oplaty check (rodzajoplaty in ('wynajem', 'czynsz', 'prad', 'gaz', 'media')),
+	constraint kwota_oplaty check (kwota >= 0)
+)

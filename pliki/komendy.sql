112e98723321e939b338171a202c32bc39b8800a
pawelgithub9 <pawelkonto1233@gmail.com>
2025-01-10 14:59:26 +0100

    wlasciciele i wlasciciele-meiszkania

---
+++ pliki/komendy.sql
@@ -0,0 +1,26 @@
+WLASCICIELE
+
+create table WLASCICIELE (
+	id autoincrement,
+	imie varchar(30),
+	nazwisko varchar(30),
+	pesel varchar(11),
+	plec varchar(1),
+	telefon varchar(15),
+	adreszamieszkania varchar(100)
+	constraint pk_wlasciiele primary key (id),
+	constraint plec_wlasciiele check (plec in ('K', 'M')),
+	constraint daty_wlasciiele check (dataurodzenia < datasmierci)
+)
+
+WLASCICIEL-MIESZKANIE
+
+create table WLASCICIEL-MIESZKANIE (
+	id autoincrement,
+	wlasciciel int,
+	mieszkanie int,
+	constraint pk_wlascicielmieszkanie primary key (id),
+	constraint fk_wlasciciel foreign key (wlasciciel) references WLASCICIELE (id),
+	constraint fk_mieszkanie foreign key (mieszkanie) references MIESZKANIA (id),
+)
+

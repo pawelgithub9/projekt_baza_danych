9f98ff4bf38da235834a32f0c1e1b9966e62cd17
pawelgithub9 <pawelkonto1233@gmail.com>
2025-01-10 22:40:03 +0100

    Update wlasciciel-mieszkanie.sql

--- pliki_sql/wlasciciel-mieszkanie.sql
+++ pliki_sql/wlasciciel-mieszkanie.sql
@@ -4,5 +4,5 @@ create table WLASCICIEL-MIESZKANIE (
 	mieszkanie int,
 	constraint pk_wlascicielmieszkanie primary key (id),
 	constraint fk_wlasciciel foreign key (wlasciciel) references WLASCICIELE (id),
-	constraint fk_mieszkanie foreign key (mieszkanie) references MIESZKANIA (id),
+	constraint fk_mieszkanie foreign key (mieszkanie) references MIESZKANIA (id)
 )

296d420161c702a99c4894b3237f3c526f389098
Droxxon <126577407+Droxxon@users.noreply.github.com>
2025-01-16 21:04:51 +0100

    Add files via upload

---
+++ pliki_sql/dane-wlasciciele.sql
@@ -0,0 +1,14 @@
+insert into WLASCICIELE (imie, nazwisko, pesel, plec, telefon, adreszamieszkania) 
+values ("Jerzy", "Pawlak", 74041434573, "M", "+48825608998", "ul. Kwiecista 1, Łódź 93-345");
+
+insert into WLASCICIELE (imie, nazwisko, pesel, plec, telefon, adreszamieszkania) 
+values ("Przemysław", "Piotrowski", 63111180119, "M", "+48976315233", "ul. Henryka Sienkiewicza 101/109 m. 31, Łódź 90-301");
+
+insert into WLASCICIELE (imie, nazwisko, pesel, plec, telefon, adreszamieszkania) 
+values ("Bartosz", "Wojciechowski", 92053172852, "M", "+48813981317", "ul. Wolności 37C, Aleksandrów Łódzki 95-069");
+
+insert into WLASCICIELE (imie, nazwisko, pesel, plec, telefon, adreszamieszkania) 
+values ("Milena", "Szulc", 01210183045, "K", "+498498013544", "ul. Piotrkowska 114 m. 12, Łódź 90-001");
+
+insert into WLASCICIELE (imie, nazwisko, pesel, plec, telefon, adreszamieszkania) 
+values ("Krystyna", "Mróz", 69022867381, "K", "+48752925512", "ul. Łanowa 6, Andrespol 95-020");

cf9e56e1c9e2a1ec0f8302d984482f9dfe7be24f
Droxxon <126577407+Droxxon@users.noreply.github.com>
2025-01-17 00:46:24 +0100

    Add files via upload

---
+++ pliki_sql/wlascicielmieszkanie-dane.sql
@@ -0,0 +1,44 @@
+insert into WLASCICIELMIESZKANIE (wlasciciel, mieszkanie)
+values (1, 1);
+
+insert into WLASCICIELMIESZKANIE (wlasciciel, mieszkanie)
+values (1, 2);
+
+insert into WLASCICIELMIESZKANIE (wlasciciel, mieszkanie)
+values (1, 3);
+
+insert into WLASCICIELMIESZKANIE (wlasciciel, mieszkanie)
+values (2, 4);
+
+insert into WLASCICIELMIESZKANIE (wlasciciel, mieszkanie)
+values (2, 5);
+
+insert into WLASCICIELMIESZKANIE (wlasciciel, mieszkanie)
+values (2, 6);
+
+insert into WLASCICIELMIESZKANIE (wlasciciel, mieszkanie)
+values (3, 7);
+
+insert into WLASCICIELMIESZKANIE (wlasciciel, mieszkanie)
+values (3, 8);
+
+insert into WLASCICIELMIESZKANIE (wlasciciel, mieszkanie)
+values (3, 9);
+
+insert into WLASCICIELMIESZKANIE (wlasciciel, mieszkanie)
+values (4, 10);
+
+insert into WLASCICIELMIESZKANIE (wlasciciel, mieszkanie)
+values (4, 11);
+
+insert into WLASCICIELMIESZKANIE (wlasciciel, mieszkanie)
+values (4, 12);
+
+insert into WLASCICIELMIESZKANIE (wlasciciel, mieszkanie)
+values (5, 13);
+
+insert into WLASCICIELMIESZKANIE (wlasciciel, mieszkanie)
+values (5, 14);
+
+insert into WLASCICIELMIESZKANIE (wlasciciel, mieszkanie)
+values (5, 15);

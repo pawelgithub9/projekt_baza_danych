0a204cf4a516c1d8f9efa3abb37d831e0dbb0abf
pawelgithub9 <pawelkonto1233@gmail.com>
2025-01-16 20:32:15 +0100

    Update oplaty.sql

--- pliki_sql/oplaty.sql
+++ pliki_sql/oplaty.sql
@@ -5,7 +5,7 @@ create table OPLATY (
 	kwota decimal(4, 2),
 	data date,
 	constraint pk_oplaty primary key (id),
-	constraint fk_mieszkanie foreign key (mieszkanie) references MIESZKANIA (id),
+	constraint fk_oplaty_mieszkanie foreign key (mieszkanie) references MIESZKANIA (id),
 	constraint rodzajoplaty_oplaty check (rodzajoplaty in ('wynajem', 'czynsz', 'prad', 'gaz', 'media')),
 	constraint kwota_oplaty check (kwota >= 0)
 )

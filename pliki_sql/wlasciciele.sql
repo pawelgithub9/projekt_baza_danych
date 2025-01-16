0739e26773c0fab621724b315b9f7abd4b1c1829
pawelgithub9 <pawelkonto1233@gmail.com>
2025-01-16 21:48:16 +0100

    Update wlasciciele.sql

--- pliki_sql/wlasciciele.sql
+++ pliki_sql/wlasciciele.sql
@@ -1,12 +1,15 @@
 create table WLASCICIELE (
 	id autoincrement,
-	imie varchar(30),
-	nazwisko varchar(40),
-	pesel varchar(11),
-	plec varchar(1),
-	telefon varchar(15),
-	adreszamieszkania varchar(100),
+	imie varchar(30) not null,
+	nazwisko varchar(40) not null,
+	pesel varchar(11) not null,
+	plec varchar(1) not null,
+	telefon varchar(15) not null,
+	adreszamieszkania varchar(100) not null,
 	constraint pk_wlasciciele primary key (id),
 	constraint plec_wlasciciele check (plec in ('K', 'M')),
-	constraint pesel_wlasciciele check (len(pesel) = 11)
+	constraint pesel_wlasciciele check (len(pesel) = 11),
+	constraint unique_pesel_wlasciciele unique (pesel),
+	constraint isnum_pesel_wlasciciele check (pesel LIKE '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'),
+	constraint isnum_telefon_wlasciciele check (telefon LIKE '%[^0-9]%')
 )

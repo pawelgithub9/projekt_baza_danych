34df4de96ea7d03cf4a750eebe0094d0240730bb
Droxxon <126577407+Droxxon@users.noreply.github.com>
2025-01-17 00:43:47 +0100

    Add files via upload

---
+++ pliki_sql/wynajmy-dane.sql
@@ -0,0 +1,59 @@
+insert into WYNAJMY (mieszkanie, klient, datarozpoczecia, datazakonczenia)
+values (1, 1, "22.11.2024", "20.12.2024");
+
+insert into WYNAJMY (mieszkanie, klient, datarozpoczecia, datazakonczenia)
+values (1, 2, "02.01.2025", "01.02.2025");
+
+insert into WYNAJMY (mieszkanie, klient, datarozpoczecia, datazakonczenia)
+values (2, 1, "02.01.2025", "28.02.2025");
+
+insert into WYNAJMY (mieszkanie, klient, datarozpoczecia, datazakonczenia)
+values (2, 2, "10.10.2024", "18.12.2024");
+
+insert into WYNAJMY (mieszkanie, klient, datarozpoczecia, datazakonczenia)
+values (3, 3, "04.10.2024", "03.01.2025");
+
+insert into WYNAJMY (mieszkanie, klient, datarozpoczecia, datazakonczenia)
+values (4, 4, "06.01.2023", "11.12.2024");
+
+insert into WYNAJMY (mieszkanie, klient, datarozpoczecia, datazakonczenia)
+values (5, 4, "12.12.2024", "03.03.2025");
+
+insert into WYNAJMY (mieszkanie, klient, datarozpoczecia, datazakonczenia)
+values (5, 5, "21.05.2023", "30.11.2024");
+
+insert into WYNAJMY (mieszkanie, klient, datarozpoczecia, datazakonczenia)
+values (6, 6, "11.02.2024", "28.02.2025");
+
+insert into WYNAJMY (mieszkanie, klient, datarozpoczecia, datazakonczenia)
+values (7, 7, "14.09.2024", "28.06.2025");
+
+insert into WYNAJMY (mieszkanie, klient, datarozpoczecia, datazakonczenia)
+values (8, 8, "12.12.2023", "02.02.2025");
+
+insert into WYNAJMY (mieszkanie, klient, datarozpoczecia, datazakonczenia)
+values (9, 9, "05.05.2024", "31.10.2024");
+
+insert into WYNAJMY (mieszkanie, klient, datarozpoczecia, datazakonczenia)
+values (9, 10, "10.11.2024", "28.02.2025");
+
+insert into WYNAJMY (mieszkanie, klient, datarozpoczecia, datazakonczenia)
+values (10, 10, "14.01.2024", "09.11.2024");
+
+insert into WYNAJMY (mieszkanie, klient, datarozpoczecia, datazakonczenia)
+values (11, 11, "02.01.2025", "28.02.2025");
+
+insert into WYNAJMY (mieszkanie, klient, datarozpoczecia, datazakonczenia)
+values (12, 12, "22.03.2023", "28.02.2025");
+
+insert into WYNAJMY (mieszkanie, klient, datarozpoczecia, datazakonczenia)
+values (13, 13, "08.08.2024", "12.12.2024");
+
+insert into WYNAJMY (mieszkanie, klient, datarozpoczecia, datazakonczenia)
+values (14, 14, "01.02.2024", "25.05.2024");
+
+insert into WYNAJMY (mieszkanie, klient, datarozpoczecia, datazakonczenia)
+values (15, 14, "26.05.2024", "28.02.2025");
+
+insert into WYNAJMY (mieszkanie, klient, datarozpoczecia, datazakonczenia)
+values (15, 15, "01.03.2024", "31.03.2024");

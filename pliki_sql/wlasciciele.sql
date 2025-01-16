e89732000bbff03a918814f8c17e656657d3eed2
pawelgithub9 <pawelkonto1233@gmail.com>
2025-01-16 21:49:50 +0100

    Update wlasciciele.sql

--- pliki_sql/wlasciciele.sql
+++ pliki_sql/wlasciciele.sql
@@ -8,7 +8,6 @@ create table WLASCICIELE (
 	adreszamieszkania varchar(100) not null,
 	constraint pk_wlasciciele primary key (id),
 	constraint plec_wlasciciele check (plec in ('K', 'M')),
-	constraint pesel_wlasciciele check (len(pesel) = 11),
 	constraint unique_pesel_wlasciciele unique (pesel),
 	constraint isnum_pesel_wlasciciele check (pesel LIKE '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'),
 	constraint isnum_telefon_wlasciciele check (telefon LIKE '%[^0-9]%')

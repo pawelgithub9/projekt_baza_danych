e959432ae923269615a2fec0c4624daf918b616a
Droxxon <126577407+Droxxon@users.noreply.github.com>
2025-01-16 23:40:25 +0100

    Add files via upload

---
+++ pliki_sql/oplaty-dane.sql
@@ -0,0 +1,224 @@
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (1, "wynajem", 3000.00; 07.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (1, "czynsz", 600.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (1, "prad", 160.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (1, "gaz", 200.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (1, "media", 120.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (2, "wynajem", 4000.00; 07.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (2, "czynsz", 750.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (2, "prad", 150.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (2, "gaz", 180.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (2, "media", 130.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (3, "wynajem", 3250.00; 07.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (3, "czynsz", 700.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (3, "prad", 145.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (3, "gaz", 195.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (3, "media", 120.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (4, "wynajem", 7000.00; 07.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (4, "czynsz", 900.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (4, "prad", 200.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (4, "gaz", 220.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (4, "media", 150.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (5, "wynajem", 6000.00; 07.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (5, "czynsz", 750.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (5, "prad", 200.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (5, "gaz", 160.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (5, "media", 180.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (6, "wynajem", 2750.00; 07.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (6, "czynsz", 600.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (6, "prad", 160.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (6, "gaz", 175.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (6, "media", 100.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (7, "wynajem", 4500.00; 07.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (7, "czynsz", 850.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (7, "prad", 175.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (7, "gaz", 175.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (7, "media", 150.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (8, "wynajem", 3000.00; 07.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (8, "czynsz", 600.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (8, "prad", 150.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (8, "gaz", 165.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (8, "media", 125.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (9, "wynajem", 4000.00; 07.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (9, "czynsz", 700.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (9, "prad", 150.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (9, "gaz", 180.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (9, "media", 120.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (10, "wynajem", 3500.00; 07.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (10, "czynsz", 600.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (10, "prad", 160.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (10, "gaz", 180.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (10, "media", 120.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (11, "wynajem", 5500.00; 07.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (11, "czynsz", 650.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (11, "prad", 180.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (11, "gaz", 220.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (11, "media", 150.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (12, "wynajem", 5000.00; 07.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (12, "czynsz", 800.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (12, "prad", 180.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (12, "gaz", 250.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (12, "media", 150.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (13, "wynajem", 6500.00; 07.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (13, "czynsz", 850.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (13, "prad", 200.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (13, "gaz", 225.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (13, "media", 125.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (14, "wynajem", 7500.00; 07.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (14, "czynsz", 950.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (14, "prad", 230.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (14, "gaz", 180.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (14, "media", 175.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (15, "wynajem", 9000.00; 07.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (15, "czynsz", 950.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (15, "prad", 250.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (15, "gaz", 175.00; 02.01.2025);
+
+insert into OPLATY (mieszkanie, rodzajoplaty, kwota, data)
+values (15, "media", 200.00; 02.01.2025);

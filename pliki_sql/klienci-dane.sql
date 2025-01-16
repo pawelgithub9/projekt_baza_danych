3fc2660a9260bc0bb8b3769c7caf8f6761e550e3
Droxxon <126577407+Droxxon@users.noreply.github.com>
2025-01-17 00:31:55 +0100

    Add files via upload

---
+++ pliki_sql/klienci-dane.sql
@@ -0,0 +1,44 @@
+insert into KLIENCI (imie, nazwisko, pesel, telefon)
+values ("Ryszard", "Baran", 56021321373, "+48926156977");
+
+insert into KLIENCI (imie, nazwisko, pesel, telefon)
+values ("Albert", "Sobczak", 72092226791, "+48867937792");
+
+insert into KLIENCI (imie, nazwisko, pesel, telefon)
+values ("Bogumił", "Lis", 68121263013, "+48999081456");
+
+insert into KLIENCI (imie, nazwisko, pesel, telefon)
+values ("Aureliusz", "Mróz", 77032973056, "+48997997997");
+
+insert into KLIENCI (imie, nazwisko, pesel, telefon)
+values ("Jakub", "Nowak", 99082783738, "+48992018921");
+
+insert into KLIENCI (imie, nazwisko, pesel, telefon)
+values ("Ignacy", "Marciniak", 94060173619, "+48551519134");
+
+insert into KLIENCI (imie, nazwisko, pesel, telefon)
+values ("Kazimierz", "Głowacki", 85052217392, "+48516063408");
+
+insert into KLIENCI (imie, nazwisko, pesel, telefon)
+values ("Miron", "Borkowski", 72100895274, "+48591538712");
+
+insert into KLIENCI (imie, nazwisko, pesel, telefon)
+values ("Olaf", "Tomaszewski", 80010183756, "+48952158387");
+
+insert into KLIENCI (imie, nazwisko, pesel, telefon)
+values ("Agata", "Kubiak", 96072185024, "+48787058719");
+
+insert into KLIENCI (imie, nazwisko, pesel, telefon)
+values ("Joanna", "Czarnecka", 89091690567, "+48695545754");
+
+insert into KLIENCI (imie, nazwisko, pesel, telefon)
+values ("Malwina", "Kaźmierczak", 71082594781, "+48607797831");
+
+insert into KLIENCI (imie, nazwisko, pesel, telefon)
+values ("Zuzanna", "Szulc", 99123199989, "+48599999999");
+
+insert into KLIENCI (imie, nazwisko, pesel, telefon)
+values ("Florencja", "Jankowska", 69062193648, "+48607393178");
+
+insert into KLIENCI (imie, nazwisko, pesel, telefon)
+values ("Jolanta", "Cieślak", 81090983521, "+48920716416");

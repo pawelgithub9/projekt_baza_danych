e5affdae096048de556a17e749fb8f2ca06ed040
Droxxon <126577407+Droxxon@users.noreply.github.com>
2025-01-16 22:30:18 +0100

    Update mieszkania-dane.sql

--- pliki_sql/mieszkania-dane.sql
+++ pliki_sql/mieszkania-dane.sql
@@ -8,37 +8,37 @@ insert into MIESZKANIA (adres, liczbapokoi, metraz, pietro, typbudynku, umeblowa
 values ("ul. Libijska 5, Łódź 93-482", 6, 92, 0, "Dom jednorodzinny", 1);
 
 insert into MIESZKANIA (adres, liczbapokoi, metraz, pietro, typbudynku, umeblowanie)
-values ("ul. Profilowa 4, Łódź 93-447", 14, 216, 0, "Dom jednorodzinny", 0)
+values ("ul. Profilowa 4, Łódź 93-447", 14, 216, 0, "Dom jednorodzinny", 0);
 
 insert into MIESZKANIA (adres, liczbapokoi, metraz, pietro, typbudynku, umeblowanie)
 values ("ul. Okołowicza 30, Krzywiec 95-070", 12, 200, 0, "Dom jednorodzinny", 0);
 
 insert into MIESZKANIA (adres, liczbapokoi, metraz, pietro, typbudynku, umeblowanie)
-values ("ul. Zaolziańska 13 m. 10, Łódź 93-545", 4, 56, 3, "Blok", 1)
+values ("ul. Zaolziańska 13 m. 10, Łódź 93-545", 4, 56, 3, "Blok", 1);
 
 insert into MIESZKANIA (adres, liczbapokoi, metraz, pietro, typbudynku, umeblowanie)
-values ("ul. Targowa 12 m. 7, Łódź 90-023", 5, 60, 2, "Kamienica", 0)
+values ("ul. Targowa 12 m. 7, Łódź 90-023", 5, 60, 2, "Kamienica", 0);
 
 insert into MIESZKANIA (adres, liczbapokoi, metraz, pietro, typbudynku, umeblowanie)
-values ("ul. Julianowska 1 m. 86, Łódź 91-473" 6, 68, 8, "Blok", 1)
+values ("ul. Julianowska 1 m. 86, Łódź 91-473" 6, 68, 8, "Blok", 1);
 
 insert into MIESZKANIA (adres, liczbapokoi, metraz, pietro, typbudynku, umeblowanie)
-values ("ul. Nastrojowa 66 m. 4, Łódź 91-496", 7, 88, 2, "Blok", 1)
+values ("ul. Nastrojowa 66 m. 4, Łódź 91-496", 7, 88, 2, "Blok", 1);
 
 insert into MIESZKANIA (adres, liczbapokoi, metraz, pietro, typbudynku, umeblowanie)
-values ("ul. Aleksandrowska 100 m. 21, Łódź 91-224", 6, 72, 7, "Blok", 0)
+values ("ul. Aleksandrowska 100 m. 21, Łódź 91-224", 6, 72, 7, "Blok", 0);
 
 insert into MIESZKANIA (adres, liczbapokoi, metraz, pietro, typbudynku, umeblowanie)
-values ("ul. Młynarska 41, Andrespol 95-020", 12, 152, 0, "Dom szeregowy", 1)
+values ("ul. Młynarska 41, Andrespol 95-020", 12, 152, 0, "Dom szeregowy", 1);
 
 insert into MIESZKANIA (adres, liczbapokoi, metraz, pietro, typbudynku, umeblowanie)
-values ("ul. Macieja Rataja 110A, Łódź 92-637", 8, 96, 0, "Dom szeregowy", 0)
+values ("ul. Macieja Rataja 110A, Łódź 92-637", 8, 96, 0, "Dom szeregowy", 0);
 
 insert into MIESZKANIA (adres, liczbapokoi, metraz, pietro, typbudynku, umeblowanie)
-values ("ul. Tulipanowa 2, Konstantynów Łódzki 95-050", 9, 102, 0, "Dom jednorodzinny", 0)
+values ("ul. Tulipanowa 2, Konstantynów Łódzki 95-050", 9, 102, 0, "Dom jednorodzinny", 0);
 
 insert into MIESZKANIA (adres, liczbapokoi, metraz, pietro, typbudynku, umeblowanie)
 values ("ul. Rafałki 18, Aleksandrów Łódzki 95-069", 13, 168, 0, "Dom jednorodzinny", 1)
 
 insert into MIESZKANIA (adres, liczbapokoi, metraz, pietro, typbudynku, umeblowanie)
-values ("ul. Kameliowa 20, Rąbień 95-070", 18, 216, 0, "Dom jednorodzinny", 1)
+values ("ul. Kameliowa 20, Rąbień 95-070", 18, 216, 0, "Dom jednorodzinny", 1);

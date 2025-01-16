ec40ef3a0202f61942339e6afd92a233aad74588
pawelgithub9 <pawelkonto1233@gmail.com>
2025-01-16 20:33:06 +0100

    Update wynajmy.sql

--- pliki_sql/wynajmy.sql
+++ pliki_sql/wynajmy.sql
@@ -5,7 +5,7 @@ create table WYNAJMY (
 	datarozpoczecia date,
 	datazakonczenia date,
 	constraint pk_wynajmy primary key (id),
-	constraint fk_mieszkanie foreign key (mieszkanie) references MIESZKANIA (id),
-	constraint fk_klient foreign key (klient) references KLIENCI (id),
+	constraint fk_wynajmy_mieszkanie foreign key (mieszkanie) references MIESZKANIA (id),
+	constraint fk_wynajmy_klient foreign key (klient) references KLIENCI (id),
 	constraint daty_wynajmy check (datarozpoczecia < datazakonczenia)
 )

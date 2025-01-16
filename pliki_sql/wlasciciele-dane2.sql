0584e7c4b5dcdac1644d75b998b3d5796fdaad08
Droxxon <126577407+Droxxon@users.noreply.github.com>
2025-01-17 00:51:16 +0100

    Add files via upload

---
+++ pliki_sql/wlasciciele-dane2.sql
@@ -0,0 +1,2 @@
+insert into WLASCICIELE (imie, nazwisko, pesel, plec, telefon, adreszamieszkania)
+values ("Wioletta", "Zalewska", 98012219267, "K", "+48802481314", "ul. Zamkowa 32, Starowa Góra 95-030");
